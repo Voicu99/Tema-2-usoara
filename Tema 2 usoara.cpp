55005dfbb6907f066e4af054194389199d22ce4e
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-16 21:45:55 +0300

    Add project files.

---
+++ Tema 2 usoara.cpp
@@ -0,0 +1,26 @@
+﻿// Tema 2 usoara.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() {
+    // Declarație a variabilelor
+    int var1, var2;
+
+    // Citirea valorilor de la tastatură
+    std::cout << "Introduceți valoarea pentru var1: ";
+    std::cin >> var1;
+
+    std::cout << "Introduceți valoarea pentru var2: ";
+    std::cin >> var2;
+
+    // Afișarea valorilor pe ecran
+    std::cout << "Valorile introduse pentru var1 și var2 sunt: \n";
+    std::cout << "var1 = " << var1 << "\n";
+    std::cout << "var2 = " << var2 << "\n";
+
+    return 0;
+}
+
+
+
